8163ac4e12755d80ebcc4042bb73a94f832e836b
Ruslan Ilderkhanov <ruslan_ild@mail.ru>
2019-03-24 18:57:36 +0200

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,29 @@
+// Homework #1.1
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result = (x * y + 5 * z + x - 1);
+
+console.log(result);
+
+// Homework #1.2
+
+let algebra = 5;
+let geography = 5;
+let physics = 4;
+let average;
+
+average = ((algebra + geography + physics)/3);
+
+console.log(average);
+
+// Homework #1.3
+
+let name = 'Руслан';
+
+let message = (`Привет, мир! Меня зовут ${name}!`);
+
+console.log(message);
