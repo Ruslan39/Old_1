6e1eddeea66333aa2d1a6380560c680d0f0f6932
Ruslan Ilderkhanov <ruslan_ild@mail.ru>
2019-04-07 21:09:51 +0200

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -13,15 +13,30 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || 
-        percent.length == 0 || contribution.length == 0 || amount.length == 0 ||
-        percent.length == ' ' || contribution.length == 0 || amount.length == 0)  {
-        alert(`Повторите ввод`);
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || 
+      percent.length == 0 || contribution.length == 0 || amount.length == 0 ||
+      percent.includes(' ') || contribution.includes(' ') || amount.includes(' ')) {
+        alert(`Для ввода данных используйте только 0123456789 и "." для десятых долей. Например 6.5; 900000`);
     }
-    
-    //|| )
 
-    //return totalAmount;
+  date = new Date(date);
+  let currentDate = new Date();
+  let millisecondsInMonth = 1000 * 60 * 60 * 24 * 29.3;
+
+  if ((date - currentDate) < 0) {
+    return alert('Дата окончания ипотеки уже в прошлом. Проверьте, пожалуйста, правильность ввода даты');
+  } else if ((date - currentDate) < millisecondsInMonth) {
+    return alert('Минимальный срок ипотеки - 1 месяц. Проверьте, пожалуйста, правильность ввода даты');
+  }
+
+  let сredit = +amount - +contribution;
+  let monthPercent = (+percent / 100 ) / 12;    
+  let n = Math.floor((date - currentDate) / millisecondsInMonth);
+  let monthPayment = сredit * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent), n) - 1 ) ) );
+  let totalAmount = monthPayment * n;
+
+  console.log(`Общая сумма ипотеки сроком на ${n} месяцев составляет: ${totalAmount.toFixed(2)} руб.`);
+  return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -32,13 +47,14 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
-}
+    name = String(name);
 
-/*Проконтролируйте корректность введенных данных.
-Посчитайте сумму, которую необходимо вернуть банку. (сумма кредита минус первоначальный взнос)
-Ежемесячная оплата рассчитывается по формуле: Платеж=S*(P+P/(((1+P)^n)-1)), где: S - сумма кредита, 
-P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
-Посчитайте общую сумму, которую придется заплатить клиенту.
-Выведите результат в консоль, а также верните его из функции.*/
+    if ((name.length === 0) || (name == 'undefined') || (name == 'null')) {
+        name = 'Аноним';
+    }
+
+    console.log(`Привет, мир! Меня зовут ${name}.`);
+
+    let greeting = (`Привет, мир! Меня зовут ${name}.`);
+    return greeting;
+}
