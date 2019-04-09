5f6a59c9d2268ea825a3520c89ac257a345b9538
Ruslan Ilderkhanov <ruslan_ild@mail.ru>
2019-04-09 21:40:36 +0200

    first commit object-prototype

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
 
@@ -7,7 +9,38 @@ function initCheckBirthday() {
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+    let now = new Date();
+    let date = new Date(birthday);
+
+    birthday = +date;
+    let diff = now - birthday;
+
+    let millesecondsInYear, averageMillesecInYear;
+    let sumMillesecInYear = 0;
+    let years = 0;
+    let age;
+
+    for (let i = date.getFullYear(); i <= now.getFullYear(); i++ ) {
+        if (((i % 4 == 0) && (i % 100 != 0)) || ((i % 100 == 0) && (i % 400 == 0))) {
+            millesecondsInYear = 1000 * 60 * 60 * 24 * 366;
+        } else {
+            millesecondsInYear = 1000 * 60 * 60 * 24 * 365;
+        }        
+        sumMillesecInYear += millesecondsInYear;
+        years++;      
+    }
+
+    averageMillesecInYear = sumMillesecInYear / years;
+    
+    age = Math.floor(diff / averageMillesecInYear);
+            
+    if (age >= 18) {
+        console.log(`Вам уже ${age} лет и Вы вправе заказывать что пожелаете!`);
+        return true;
+    } else {
+        console.log(`Сударь, Вам только ${age} лет. Крепче кефира ничего предложить не могу...`);
+        return false;
+    }    
 }
 
 function initPrintAnimalSound() {
@@ -20,8 +53,14 @@ function initPrintAnimalSound() {
     document.getElementById('sound').innerHTML = result;   
 }
 
-function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+function getAnimalSound(animal) {    
+    let sound = animal.sound;
+    
+    if (typeof animal === undefined) {
+        return null;
+    } else if (typeof animal !== undefined) {
+        return sound;
+    }    
 }
 
 function initCalculateStatement() {
@@ -35,5 +74,16 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+    let k = 0;
+    let sumMarks = 0;
+
+    for (let mark in marks) {        
+        sumMarks += +marks[mark];
+        k++;
+    }
+
+    let average = sumMarks / k;
+    let roundedAverage = Math.round(average);
+    
+    return roundedAverage;
 }
