11efd705f753556dbbd10f3af0012cf161352d3f
Ruslan Ilderkhanov <ruslan_ild@mail.ru>
2019-04-25 12:56:59 +0200

    Fixed: 1. compareArrays() - compact recording 2. memorize() - actions with empty array was deleted 3. limit overrun (unshift/pop)

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -4,42 +4,35 @@ function compareArrays(arr1, arr2) {
     if ((arr1 === undefined) || (arr2 === undefined) || (arr1.length != arr2.length)) {            
         return false;
     }
-    function isEqual(item, i, arr1) {
-        return arr1[i] === arr2[i];
-    }
-    return arr1.every(isEqual);
+    
+    return arr1.every((item, i) => item === arr2[i]);
 }
 
+let results = [];
+
 function memorize(fn, limit) {    
-    let results = [];
-    
+        
     return function(...rest) {
         const args = [...rest];        
 
-        if (results.length == 0) {           
+        const parser = results.find(item => compareArrays(item.args, args));
+            
+        if (parser) {
+            return `Результат из памяти: ${parser.result}`;
+        } else {
+            console.log(`Вычисляю новое значение функции...`);
             results.unshift({
                 args,
                 result: fn(...rest)
             })
-        } else {
-            const parser = results.find(item => compareArrays(item.args, args));
-            
-            if (parser) {
-                return parser.result;
-            } else {
-                results.unshift({
-                    args,
-                    result: fn(...rest)
-                })
 
-                if (results.length > limit) {
-                    results.shift();
-                }
-
-                return results;
+            if (results.length > limit) {
+                results.pop();
             }
-        }    
-    }     
+
+            return results;
+        }
+    }    
 }
 
 const sum = function(...rest) {
@@ -50,7 +43,7 @@ const sum = function(...rest) {
     return (sum);
 }
 
-const mSum = memorize(sum, 10);
+const mSum = memorize(sum, 3);
 
 console.log(compareArrays([8, 9], [6]));
 console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
@@ -58,17 +51,9 @@ console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]));
 console.log(compareArrays([1, 2, 3], [2, 3, 1]));
 console.log(compareArrays([8, 1, 2], [8, 1, 2]));
 
-console.log(mSum(3, 4, 5, 6));
-console.log(mSum(3, 4, 5, 6));
-console.log(mSum(3, 4, 556, 6));
-console.log(mSum(3, 4, 556, 6));
-console.log(mSum(3, 455, 5, 6));
-console.log(mSum(3, 4, 5));
-console.log(mSum(3, 5, 6));
-console.log(mSum(3));
-console.log(mSum(3, 4, 5, 6, 4, 5, 6));
-console.log(mSum(32, 42, 25, 6));
-console.log(mSum(3, 4, 55, 56));
-console.log(mSum(3, 64, 5, 61));
-console.log(mSum(3, 74, 5, 67, 123));
-console.log(mSum(3, 74, 53, 67, 123));
+console.log(mSum(3, 4, 5, 6)); //18
+console.log(mSum(3, 4, 5, 6)); //18 
+console.log(mSum(3, 5, 6)); //14
+console.log(mSum(4, 6)); //10
+console.log(mSum(32, 68)); //100
+console.log(mSum(3, 4, 5, 6)); //18
